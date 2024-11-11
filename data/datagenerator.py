e467d11d100a1fcc61cdb650ecf985aa15d04fcd
Jesse <jesseabe.rodrigues@gmail.com>
2024-11-11 14:44:16 -0300

    update: datagenerator created

---
+++ data/datagenerator.py
@@ -0,0 +1,47 @@
+from faker import Faker
+import pandas as pd
+import random
+
+# Inicializando Faker
+faker = Faker('pt_BR')
+
+# Definindo algumas categorias e produtos para simular
+negocios = ['B2B', 'B2C']
+gerencias = ['Moveis', 'Calcado', 'Construcao', 'Exportacao']
+tecnologias = ['Aquosos', 'Solventes', 'Hot Melt']
+produtos = {
+    'Aquosos': ['Produto 2156', 'Produto 5149', 'Produto 5649'],
+    'Solventes': ['Produto 1126', 'Produto 1254', 'Produto 9678'], 
+    'Hot Melt': ['Produto 1314', 'Produto 1615', 'Produto 1821']
+}
+
+# Função para gerar dados de DRE
+def gerar_dados_forecast(num_linhas=200):
+    dados = []
+    for _ in range(num_linhas):
+        negocio = random.choice(negocios)
+        gerencia = random.choice(gerencias)
+        tecnologia = random.choice(tecnologias)
+        produto = random.choice(produtos[tecnologia])
+        
+        receita_liquida = round(random.uniform(500, 50000), 2)
+        
+        linha = {
+            'Data': faker.date_this_year(),
+            'Negocio': negocio,
+            'Gerencia': gerencia,
+            'Tecnologia': tecnologia,
+            'Produto': produto,
+            'Receita Liquida': receita_liquida,
+        }
+        dados.append(linha)
+    
+    return pd.DataFrame(dados)
+
+
+if __name__ == "__main__":
+    df_re = gerar_dados_forecast(1000)
+    df_re.to_csv("data/df_re.csv")
+
+    df_or = gerar_dados_forecast(1000)
+    df_or.to_csv("data/df_or.csv")
