9e273cfd9bb66f919b00b1c02dbdbbb40cf2022d
Jesse <jesseabe.rodrigues@gmail.com>
2024-11-19 09:32:40 -0300

    update

--- src/frontend/pages/app.py
+++ src/frontend/pages/app.py
@@ -3,18 +3,21 @@ import os
 import sys
 from upload_page import upload_page
 from display_page import display_page
+from dash import dashboard
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
 def main():
     st.title("Dashboard Navigation")
     
     # Navigation Menu
-    page = st.radio("Navigate to:", ["Upload Arquivos", "Preencher Forecast"])
+    page = st.radio("Navigate to:", ["Upload Arquivos", "Preencher Forecast", "Dashboard"])
     
     if page == "Upload Arquivos":
         upload_page()  # Call the function from upload_page.py
     elif page == "Preencher Forecast":
         display_page()  # Call the function from display_page.py
+    elif page == "Dashboard":
+        dashboard()
 
 if __name__ == "__main__":
     main()
--- src/frontend/pages/dash.py
+++ src/frontend/pages/dash.py
@@ -9,23 +9,36 @@ import plotly.express as px
 from datetime import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
-st.title("Dados Realizado vs Orçado")
+def dashboard():
+    st.title("Dashboard de resultados")
 
-def load_data(table_name):
-    con = sqlite3.connect('data/forecast.db')
-    df = pd.read_sql(f'SELECT * FROM {table_name}', con)
-    con.close()
-    return df
+    def load_data(table_name):
+        con = sqlite3.connect('data/forecast.db')
+        df = pd.read_sql(f'SELECT * FROM {table_name}', con)
+        con.close()
+        return df
 
-realizado = load_data("realizado")
+    realizado = load_data("realizado")
 
-df_realizado_agrupado = realizado.groupby(["Ano", "Mes"])["Receita_Liquida"].sum()
-df_realizado_agrupado = df_realizado_agrupado.reset_index()
+    df_realizado_agrupado = realizado.groupby(["Ano", "Mes"])["Receita_Liquida"].sum()
+    df_realizado_agrupado = df_realizado_agrupado.reset_index()
 
-#Grafico de linha do Realizado VS Orcado
-figure(figsize=(14,8))
-plt.plot(df_realizado_agrupado["Mes"], df_realizado_agrupado["Receita_Liquida"])
-plt.xlabel("Mês")
-plt.ylabel("Receita Líquida")
-plt.title("Receita Líquida RE por mês")
-st.pyplot(plt)
+    #Grafico de linha do Realizado por mês
+    figure(figsize=(14,8))
+    plt.plot(df_realizado_agrupado["Mes"], df_realizado_agrupado["Receita_Liquida"])
+    plt.xlabel("Mês")
+    plt.ylabel("Receita Líquida")
+    plt.title("Receita Líquida RE por mês")
+    st.pyplot(plt)
+
+    df_realizado_uen = realizado.groupby(["Ano", "Mes", "Negocio"])["Receita_Liquida"].sum()
+    df_realizado_uen = df_realizado_uen.reset_index()
+
+    figure(figsize=(14,8))
+    plt.bar(df_realizado_uen["Negocio"], df_realizado_uen["Receita_Liquida"])
+    plt.xlabel("Mês")
+    plt.ylabel("Receita Líquida")
+    plt.title("Receita Líquida RE por Negócio")
+    st.pyplot(plt)
+
+    dfagregado = load_data("dfagregado")
